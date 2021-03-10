2e7a066ceda31761581159656acc465eab430cb0
Codelta <valentin.pretat@gmail.com>
2021-03-10 09:34:52 +0100

    day 4 progress bar

--- 2015/day-4.py
+++ 2015/day-4.py
@@ -1,9 +1,23 @@
 import hashlib
 
 input = "yzbqklnj"
-number_max = 10000000                   # Pour éviter une boucle infinie
+number_max = 1000000                    # Pour éviter une boucle infinie
 found = False                           # Pour savoir à la fin du programme si le résultat est trouvé
 number_of_zeros = 6                     # Nombre de zéros à trouver au début du md5
+
+progress_bar_width = 50                 # Taille de la progress bar
+
+def print_progress(i):
+    # Calculer le pourcentage de la recherche
+    percentage = i / number_max * 100
+    current_progress = int(progress_bar_width * percentage / 100)
+    # Imprimer la progressbar
+    for j in range(current_progress):
+        print("#", end='')
+
+    for j in range(progress_bar_width - current_progress):
+        print("_", end='')
+    print("", end="\r")
 
 # Foreach incrémentant un integer
 for i in range(number_max):
@@ -18,6 +32,7 @@ for i in range(number_max):
         print("Trouvé à " + str(i / number_max * 100) + "%")
         found = True
         break
+    print_progress(i)
 
 if not found:
     print("Aucun nombre n'a été trouvé dans l'intervalle 0-" + str(number_max))
