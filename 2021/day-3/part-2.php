fac53d7a5b13a4b51a8c6b2dcb3cd56273e22623
Codelta <valentin.pretat@gmail.com>
2021-12-03 23:07:01 +0100

    2021 day 3 part 2 improvements

--- 2021/day-3/part-2.php
+++ 2021/day-3/part-2.php
@@ -9,46 +9,41 @@ fclose($file);
 $criterias = [
     "oxy" => [
         "data" => $data,
-        "number" => 1,
+        "prefferedNumber" => 1,
     ],
     "co2" => [
         "data" => $data,
-        "number" => 0,
+        "prefferedNumber" => 0,
     ]
 ];
 
+function filterCrt($line) {
+    global $sum, $mostCommonBit, $i, $crt;
+    // If we have the same amount of 0s and 1s
+    if ($sum == sizeof($crt['data']) / 2)
+        // We return true if the current bit == the bit of the criteria
+        return $line[$i] == $crt['prefferedNumber'];
+    return ($line[$i] == $crt['prefferedNumber']) == $mostCommonBit;
+};
+
+function reduceCrt($sum, $line) {
+    global $i;
+    return $sum += intval($line[$i]);
+};
 
 // For each bit
 for ($i = 0; $i < strlen($data[0]); $i++) {
     // For each criteria
     foreach ($criterias as $key => $crt) {
-        // If we're already at 1 final answer, we continue
-        if (count($crt['data']) == 1) continue;
-
-        // Sum the nth bit of each line
-        $sum = 0;
-        foreach ($crt['data'] as $line) {
-            $sum += intval($line[$i]);
-        }
-        $moreOnes = $sum > sizeof($crt['data']) / 2;
-        $equals1And0 = $sum == sizeof($crt['data']) / 2;
-
-        // Filter the array to keep only the lines we want
-        $criterias[$key]['data'] = array_filter($crt['data'], function ($line) use (
-            $moreOnes, $equals1And0, $crt, $i) {
-            if ($equals1And0) { // If we have the same amount of 0s and 1s
-                // We return true if the current bit == the bit of the criteria
-                return $line[$i] == $crt['number'];
-            }
-            return ($line[$i] == $crt['number']) == $moreOnes;
-        });
+        if (count($crt['data']) == 1) break;
+        $sum = array_reduce($crt['data'], "reduceCrt", 0);
+        $mostCommonBit = $sum > sizeof($crt['data']) / 2;
+        $criterias[$key]['data'] = array_filter($crt['data'], "filterCrt");
     }
-}
+};
 
-$answer = (
-    bindec(reset($criterias['oxy']['data']))
-     * 
-    bindec(reset($criterias['co2']['data']))
-);
+$answer = (bindec(reset($criterias['oxy']['data']))
+    *
+    bindec(reset($criterias['co2']['data'])));
 
 echo $answer;
